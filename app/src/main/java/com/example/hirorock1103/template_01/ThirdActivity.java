18527bc244ae1ed47bcc935ddd85adf889f06f2d
hirorock1103 <mdiz1103@gmail.com>
2019-03-15 17:40:16 +0900

    add riple

--- app/src/main/java/com/example/hirorock1103/template_01/ThirdActivity.java
+++ app/src/main/java/com/example/hirorock1103/template_01/ThirdActivity.java
@@ -3,15 +3,18 @@ package com.example.hirorock1103.template_01;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.example.hirorock1103.template_01.Dialog.DialogSourceView;
 
 public class ThirdActivity extends AppCompatActivity {
 
     private Button sample;
+    private LinearLayout layoutBt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,14 @@ public class ThirdActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //oncreate dialog
                 createDialog();
+            }
+        });
+
+        layoutBt = findViewById(R.id.sample5);
+        layoutBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
